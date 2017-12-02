265038cca573d52fc0e00b74b5e4d5904081f059
LittleJeki <291102063@qq.com>
2017-12-02 16:42:21 +0800

    牌组

--- app/src/main/java/com/ooad/a21point/GameModels/Pile.java
+++ app/src/main/java/com/ooad/a21point/GameModels/Pile.java
@@ -1,9 +1,32 @@
 package com.ooad.a21point.GameModels;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by 10040 on 2017/12/2.
  */
 
 public class Pile  {
 
+    ArrayList<Card> card=new ArrayList<Card>();
+    private int location=0;
+
+    public Pile() {
+        //初始化牌组中所有牌的点数与花色
+        for(int i=0;i<13;i++)
+            for(int j=0;j<4;j++)
+                card.add(new Card(j+1,i+1));
+
+    }
+
+    public Card getCard(){
+        location=location+1;
+        return card.get(location-1);
+    }
+
+    public void reset(){
+        Collections.shuffle(card);
+        location=0;
+    }
 }
