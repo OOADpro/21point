34d5875697632419a8f3fe72b14c5dc6638a0ada
LittleJeki <291102063@qq.com>
2017-12-03 11:03:55 +0800

    gamemanager的完善

--- app/src/main/java/com/ooad/a21point/GameModels/GameManager.java
+++ app/src/main/java/com/ooad/a21point/GameModels/GameManager.java
@@ -1,6 +1,5 @@
 package com.ooad.a21point.GameModels;
 
-import android.provider.BlockedNumberContract;
 
 import java.util.ArrayList;
 
@@ -37,51 +36,59 @@ public class GameManager {
         return mplayer.get(0);
     }
 
-    public void doAiBanker(){
-
+    public void doAiBanker(Hand hand){                          //人工智能·庄家
+        while (!hand.isStand()) {
+            int bpoint = mbanker.getHands().get(0).getPoint();  //庄家的点数
+            if (bpoint >= 21)
+                hand.stand();
+            else {
+                for (Hand phand : mplayer.get(0).getHands()) {
+                    int ppoint = phand.getPoint();              //闲家的点数
+                    if (ppoint > bpoint)
+                        hand.getAClosedCard();                  //当庄家手牌点数小于闲家手牌且没有爆掉，抽一张暗牌
+                }
+            }
+        }
     }
 
     public void makebet(Player player,int bet){
         player.makeBet(bet);
     }
 
-    public void doublebet(Player player,Hand hand){
-        player.makeBet(hand.getBet());
-        hand.doubleBet();
-    }
-
-    public void hit(Player player,Hand hand){
-
-        hand.getAnOpenCard();
-        while(hand.getPoint()>=21)
+    public void hit(Player player,Hand hand){       //拿牌
+        hand.getAnOpenCard();                       //get a opencard
+        while(hand.getPoint()>=21)                  //stand when point is over 21
             stand(player,hand);
     }
 
-    public void split(Player player){
-
+    public void doublebet(Player player,Hand hand){ //加倍 hit once and then stand
+        player.makeBet(hand.getBet());
+        hand.doubleBet();                           //handle bet
+        hit(player,hand);
+        stand(player,hand);
     }
 
-    public ArrayList<Hand> judge(){
-        ArrayList<Hand> winhand=new ArrayList<>();// 新建赢家的手牌，空
+    public void split(Player player){               //分牌
+        player.split(mpile);
+    }
+
+    public ArrayList<Hand> judge(){                 //判断胜负
+        ArrayList<Hand> winhand=new ArrayList<>();  // 新建赢家的手牌，初始为空
         int bpoint=mbanker.getHands().get(0).getPoint();//庄家的点数
 
         for(Hand phand:mplayer.get(0).getHands()) {
             int ppoint=phand.getPoint();                //闲家的点数
-            if (ppoint > bpoint) {
+            if (ppoint > bpoint && ppoint <= 21) {
                 mplayer.get(0).winchip(phand.getBet()); //闲家胜利，获得相应的筹码
                 winhand.add(phand);
-            } else if (ppoint < bpoint)
+            } else if (ppoint < bpoint && bpoint <= 21) //庄家胜利
                 winhand.add(mbanker.getHands().get(0));
         }
         return winhand;
     }
 
-    public void stand(Player player,Hand hand){
-        hand.stand();
-
-        while (player.isStand()){
-            doAiBanker();
-            judge();
-        }
+    public void stand(Player player,Hand hand){         //停牌
+        while (!hand.isStand())
+            hand.stand();
     }
 }
