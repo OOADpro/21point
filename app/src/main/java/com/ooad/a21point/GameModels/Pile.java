77b9fe69efc38c6ecff20ca049a5701ed8499507
LittleJeki <291102063@qq.com>
2018-01-04 15:06:33 +0800

    cc

--- app/src/main/java/com/ooad/a21point/GameModels/Pile.java
+++ app/src/main/java/com/ooad/a21point/GameModels/Pile.java
@@ -1,5 +1,7 @@
 package com.ooad.a21point.GameModels;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -11,22 +13,27 @@ public class Pile  {
 
     private ArrayList<Card> mCard = new ArrayList<>();
     private int mLocation =0;
+    private ArrayList<Integer> list=new ArrayList<>();
 
     public Pile() {
         //初始化牌组中所有牌的点数与花色
         for(int i=0;i<13;i++)
             for(int j=0;j<4;j++)
                 mCard.add(new Card(j+1,i+1));
-
+        for(int i=0;i<52;i++)
+            list.add(i);
+        reset();
     }
 
     public Card getCard(){
         if(mLocation<52) {
             mLocation = mLocation + 1;
-            return mCard.get(mLocation - 1);
+            int mcardlocation=list.get(mLocation-1);
+            return mCard.get(mcardlocation);
         }
         else
-            return null;
+            reset();
+            return getCard();
     }
 
 
@@ -36,7 +43,10 @@ public class Pile  {
         for(int i=0;i<50;i++) {
             int ranA = (int) (Math.random() * 52);
             int ranB = (int) (Math.random() * 52);
-            Collections.swap(mCard,ranA,ranB);
+//            Log.d("ranA",String.valueOf(ranA));
+//            Log.d("ranB",String.valueOf(ranB));
+            Collections.swap(list,ranA,ranB);
+//            Log.d("liatrana",String.valueOf(list.get(ranA))+String.valueOf(list.get(ranB)));
         }
         mLocation =0;
 
